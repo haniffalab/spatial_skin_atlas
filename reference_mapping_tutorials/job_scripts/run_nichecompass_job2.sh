66f81dd15db8d14b760fdb4ddcd08c1e7ac9c707
Lloyd Steele <ls864@sanger.ac.uk>
2025-12-16 17:06:02 +0000

    Remove old job script folder

--- reference_mapping_tutorials/job_scripts/run_nichecompass_job2.sh
+++
@@ -1,5 +0,0 @@
-export MODULEPATH="/software/modules:$MODULEPATH"
-module load cellgen/nichecompass/0.3.0                
-echo "LOAD"
-python3 /nfs/users/nfs_l/ls34/spatial_skin_atlas/reference_mapping_tutorials/nichecompass_ref_mapping_pretrainedmodel.py
-echo "DONE"
--- reference_mapping_tutorials/run_nichecompass_script/run_nichecompass_job.sh
+++
@@ -1,5 +0,0 @@
-export MODULEPATH="/software/modules:$MODULEPATH"
-module load cellgen/nichecompass/0.3.0                
-echo "LOAD"
-python3 /nfs/users/nfs_l/ls34/spatial_skin_atlas/reference_mapping_tutorials/job_scripts/nichecompass_ref_mapping2.py
-echo "DONE"
